56d9a6b409158fb1ff482d393e10108d5fd3d44c
statiolake <statiolake@gmail.com>
2018-01-07 17:13:15 +0900

    init: change to open main.cpp after init

--- src/init.rs
+++ src/init.rs
@@ -1,6 +1,18 @@
 use std::io::prelude::*;
 use std::path::Path;
 use std::fs::File;
+use std::process::Command;
+
+fn spawn(name: &str) {
+    let successful = Command::new("open")
+        .arg(name)
+        .output()
+        .is_ok();
+
+    if !successful {
+        print_error!("failed to open main.cpp file. please manually open the file.");
+    }
+}
 
 pub fn main() -> bool {
     let p: &Path = "main.cpp".as_ref();
@@ -9,19 +21,22 @@ pub fn main() -> bool {
         return false;
     }
 
-    let mut f = match File::create("main.cpp") {
-        Ok(f) => f,
-        Err(_) => return false,
-    };
+    {
+        let mut f = match File::create("main.cpp") {
+            Ok(f) => f,
+            Err(_) => return false,
+        };
 
-    writeln!(f, "#include <bits/stdc++.h>").unwrap();
-    writeln!(f, "using namespace std;").unwrap();
-    writeln!(f, "int main() {{").unwrap();
-    writeln!(f, "").unwrap();
-    writeln!(f, "    return 0;").unwrap();
-    writeln!(f, "}}").unwrap();
-
+        writeln!(f, "#include <bits/stdc++.h>").unwrap();
+        writeln!(f, "using namespace std;").unwrap();
+        writeln!(f, "int main() {{").unwrap();
+        writeln!(f, "").unwrap();
+        writeln!(f, "    return 0;").unwrap();
+        writeln!(f, "}}").unwrap();
+    }
     print_created!("main.cpp");
+
+    spawn("main.cpp");
 
     return true;
 }
