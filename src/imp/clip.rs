18dedc7175f456f1cc5797a01805881002cc28e5
statiolake <statiolake@gmail.com>
2018-06-02 19:58:35 +0900

    clip: add minify feature

--- src/imp/clip.rs
+++ src/imp/clip.rs
@@ -1,5 +1,6 @@
 use clipboard::{ClipboardContext, ClipboardProvider};
 
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::mem;
@@ -14,6 +15,7 @@ use {Error, Result};
 pub fn copy_to_clipboard(file_path: &Path) -> Result<()> {
     print_copying!("{} to clipboard", file_path.display());
     let main_src = read_source_file(file_path, false)?;
+    let main_src = preprocess(main_src)?;
     set_clipboard(main_src)?;
     print_finished!("copying");
     Ok(())
@@ -102,4 +104,126 @@ fn parse_include(curr_file_path: &Path, content: String, silent: bool) -> Result
     let modified_content = modified_content.join("\n");
 
     Ok(modified_content)
+}
+
+fn preprocess(content: String) -> Result<String> {
+    let content = remove_block_comments(content);
+    let lines: Vec<String> = content.split('\n').map(|x| x.into()).collect();
+    let removed = Some(lines)
+        .map(remove_line_comments)
+        .map(remove_include_guard)
+        .unwrap()?;
+    Ok(minify(removed))
+}
+
+fn remove_block_comments(content: String) -> String {
+    let re_block_comment = Regex::new(r#"(?s)/\*.*?\*/"#).unwrap();
+    re_block_comment.replace_all(&content, "").into()
+}
+
+fn remove_line_comments(mut lines: Vec<String>) -> Vec<String> {
+    let re_line_comment = Regex::new(r#"//.*"#).unwrap();
+
+    for line in &mut lines {
+        *line = re_line_comment.replace_all(line, "").trim().into();
+    }
+    lines
+}
+
+fn remove_include_guard(mut lines: Vec<String>) -> Result<Vec<String>> {
+    let re_preprocessor_directive = Regex::new(r#"\s*#.*"#).unwrap();
+    let re_if = Regex::new(r#"\s*#\s*if"#).unwrap();
+    let re_ifndef = Regex::new(r#"\s*#\s*ifndef\s*(.*)"#).unwrap();
+    let re_define = Regex::new(r#"\s*#\s*define\s*([^(]*)"#).unwrap();
+    let re_endif = Regex::new(r#"\s*#\s*endif"#).unwrap();
+
+    let mut result = Vec::new();
+    let mut building = String::new();
+    let mut defined = HashSet::new();
+    let mut i = 0;
+
+    while i < lines.len() {
+        let find_corresponding_endif =
+            |lines: &Vec<String>, line_of_ifdef: usize| -> Result<usize> {
+                let mut curr = line_of_ifdef + 1;
+                let mut if_nest = 1;
+                while if_nest > 0 {
+                    if curr >= lines.len() {
+                        return Err(Error::new(
+                            "removing include guard",
+                            "failed to find endif matching with ifdef.",
+                        ));
+                    }
+
+                    if re_if.is_match(&lines[curr]) {
+                        if_nest += 1;
+                    } else if re_endif.is_match(&lines[curr]) {
+                        if_nest -= 1;
+                    }
+                    curr += 1;
+                }
+                Ok(curr - 1)
+            };
+
+        if re_preprocessor_directive.is_match(&lines[i]) {
+            result.push(mem::replace(&mut building, String::new()));
+            let has_enough_space_for_ifdef_define = i < lines.len() - 1;
+            if has_enough_space_for_ifdef_define && re_ifndef.is_match(&lines[i])
+                && re_define.is_match(&lines[i + 1])
+            {
+                let symbol_name = re_ifndef
+                    .captures(&lines[i])
+                    .expect("there must be ifndef!")[1]
+                    .to_string();
+                if defined.contains(&symbol_name) {
+                    i = find_corresponding_endif(&lines, i)?;
+                } else {
+                    defined.insert(symbol_name);
+                    let j = find_corresponding_endif(&lines, i)?;
+                    lines.remove(j);
+                    i += 1;
+                }
+            } else {
+                result.push(lines[i].clone());
+            }
+        } else {
+            building += &lines[i];
+        }
+
+        i += 1;
+    }
+    if !building.is_empty() {
+        result.push(building);
+    }
+    Ok(result)
+}
+
+fn minify(preprocessed_lines: Vec<String>) -> String {
+    let mut result = preprocessed_lines;
+    result = result
+        .into_iter()
+        .map(|x| x.trim().to_string())
+        .filter(|x| !x.is_empty())
+        .collect();
+    let re_whitespace_after_block_comment = Regex::new(r#"\*/\s+"#).unwrap();
+    let re_whitespace_after_semicolon = Regex::new(r#";\s+"#).unwrap();
+    let re_multiple_space = Regex::new(r#"\s+"#).unwrap();
+    let re_whitespace_around_operator =
+        Regex::new(r#"\s*(?P<op>[+\-*/%~^|&<>=,.]|<<|>>|<=|>=|==|!=|\+=|-=|\*=|/=)\s*"#).unwrap();
+    let re_whitespace_around_paren = Regex::new(r#"\s*(?P<par>[({)}])\s*"#).unwrap();
+
+    for (regex, replace) in [
+        (re_whitespace_after_block_comment, "*/"),
+        (re_whitespace_after_semicolon, ";"),
+        (re_multiple_space, " "),
+        (re_whitespace_around_operator, "$op"),
+        (re_whitespace_around_paren, "$par"),
+    ].iter()
+    {
+        for r in &mut result {
+            *r = r.trim().into();
+            *r = regex.replace_all(r, replace as &str).into();
+        }
+    }
+    result.join("\n")
 }
