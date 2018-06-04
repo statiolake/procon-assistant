63e0a1887b5a82421a517492d20c37935ee98be9
statiolake <statiolake@gmail.com>
2018-06-04 17:09:27 +0900

    cilp: use define_error_kind macro

--- src/clip.rs
+++ src/clip.rs
@@ -1,29 +1,15 @@
-use std::fmt;
-
 use imp::clip;
 use imp::srcfile;
 use imp::srcfile::SrcFile;
 
 define_error!();
-
-#[derive(Debug)]
-pub enum ErrorKind {
-    GettingSourceFileFailed,
-    CopyingClipboardFailed,
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, b: &mut fmt::Formatter) -> fmt::Result {
-        let message = match self.kind {
-            ErrorKind::GettingSourceFileFailed => "failed to get source file.",
-            ErrorKind::CopyingClipboardFailed => "failed to copy the source file to the clipboard.",
-        };
-        write!(b, "{}", message)
-    }
+define_error_kind! {
+    [GettingSourceFileFailed; (); "failed to get source file."];
+    [CopyingClipboardFailed; (); "failed to copy the source file to the clipboard."];
 }
 
 pub fn main() -> Result<()> {
     let SrcFile { file_name, .. } =
-        srcfile::get_source_file().chain(ErrorKind::GettingSourceFileFailed)?;
-    clip::copy_to_clipboard(file_name.as_ref()).chain(ErrorKind::CopyingClipboardFailed)
+        srcfile::get_source_file().chain(ErrorKind::GettingSourceFileFailed())?;
+    clip::copy_to_clipboard(file_name.as_ref()).chain(ErrorKind::CopyingClipboardFailed())
 }
