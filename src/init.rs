c1223bd601f3cad480f2a045eff80c29a1181def
statiolake <statiolake@gmail.com>
2018-11-24 12:53:16 +0900

    init: change to create new directory
    previously init command initializes project in a current directory, but
    most of the use case is to create new empty directory for problem and
    `cd` to the directory, and then run `procon-assistant init` in the
    destination directory. this is very complex style.
    
    so now init command takes one argument --- the directory name. now init
    command creates new directory named specified, and then initialize new
    project in it.

--- src/init.rs
+++ src/init.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use crate::imp::common;
 use crate::imp::config::ConfigFile;
@@ -20,7 +20,8 @@ const FILES: &[&str] = &[
 define_error!();
 define_error_kind! {
     [GettingConfigFailed; (); format!("failed to get config.")];
-    [UnknownFileType; (file_type: String); format!("unknown file type: {}", file_type)];
+    [ProjectNameNotSpecified; (); format!("project name was not specified.")];
+    [UnknownFileType; (lang: String); format!("unknown file type: {}", lang)];
     [CreateDestinationDirectoryFailed; (name: String); format!("creating directory `{}' failed.", name)];
     [CreateDestinationFailed; (name: String); format!("creating `{}' failed.", name)];
     [WriteToDestinationFailed; (name: String); format!("writing `{}' failed.", name)];
@@ -29,54 +30,90 @@ define_error_kind! {
     [OpeningEditorFailed; (); format!("failed to open editor.")];
 }
 
+struct CmdLineArgs {
+    name: String,
+    lang: Option<String>,
+}
+
+impl CmdLineArgs {
+    pub fn parse(args: Vec<String>) -> Result<CmdLineArgs> {
+        let mut args = args.into_iter();
+
+        let mut name = None;
+        let mut lang = None;
+
+        while let Some(arg) = args.next() {
+            match &*arg {
+                "-t" | "--type" => lang = args.next(),
+                _ => name = Some(arg),
+            }
+        }
+
+        name.ok_or(Error::new(ErrorKind::ProjectNameNotSpecified()))
+            .map(|name| CmdLineArgs { name, lang })
+    }
+}
+
+struct Project {
+    name: String,
+    lang: &'static Lang,
+}
+
 pub fn main(args: Vec<String>) -> Result<()> {
     let config: ConfigFile = ConfigFile::get_config().chain(ErrorKind::GettingConfigFailed())?;
 
-    // generate source code
-    let specified_file_type = args
-        .into_iter()
-        .next()
-        .unwrap_or(config.init_default_file_type.clone());
-    let file_type = langs::FILETYPE_ALIAS
-        .get(&*specified_file_type)
-        .ok_or(Error::new(ErrorKind::UnknownFileType(specified_file_type)))?;
-    let lang = langs::LANGS_MAP
-        .get(file_type)
-        .expect(&format!("internal error: unknown file type {}", file_type));
-    safe_generate(&lang, Path::new(&lang.src_file_name))?;
+    // parse command line arguments
+    let cmdargs = CmdLineArgs::parse(args)?;
+    let project = validate_arguments(&config, cmdargs)?;
+
+    let out_dir = Path::new(&project.name);
+    create_project_directory(&out_dir)?;
+
+    let path_src_file = Path::new(&project.lang.src_file_name);
+    safe_generate(&project.lang, out_dir, path_src_file)?;
 
     for file in FILES {
         let path = Path::new(file);
-        safe_generate(&lang, path)?;
+        safe_generate(&project.lang, out_dir, path)?;
     }
 
     if config.init_auto_open {
-        match config.init_open_directory_instead_of_specific_file {
-            true => common::open(&config, &["."]).chain(ErrorKind::OpeningEditorFailed())?,
-            false => common::open(&config, &["main.cpp"]).chain(ErrorKind::OpeningEditorFailed())?,
-        }
+        let open_path = if config.init_open_directory_instead_of_specific_file {
+            out_dir.display().to_string()
+        } else {
+            out_dir
+                .join(&project.lang.src_file_name)
+                .display()
+                .to_string()
+        };
+        common::open(&config, &[&open_path]).chain(ErrorKind::OpeningEditorFailed())?;
     }
 
     Ok(())
 }
 
-fn safe_generate(lang: &Lang, path: &Path) -> Result<()> {
-    if path.exists() {
+fn create_project_directory(out_dir: &Path) -> Result<()> {
+    fs::create_dir(out_dir).chain(ErrorKind::CreateDestinationDirectoryFailed(
+        out_dir.display().to_string(),
+    ))
+}
+
+fn safe_generate(lang: &Lang, out_dir: &Path, path: &Path) -> Result<()> {
+    if out_dir.join(path).exists() {
         print_info!(true, "file {} already exists, skipping.", path.display());
         return Ok(());
     }
 
-    generate(lang, path)?;
+    generate(lang, out_dir, path)?;
     print_generated!("{}", path.display());
     Ok(())
 }
 
-fn generate(lang: &Lang, path: &Path) -> Result<()> {
+fn generate(lang: &Lang, out_dir: &Path, path: &Path) -> Result<()> {
     let exe_dir = env::current_exe().unwrap();
     let exe_dir = exe_dir.parent().unwrap();
-    let template_path = exe_dir
-        .join("template")
-        .join(&path.components().collect::<PathBuf>());
+    let template_path = exe_dir.join("template").join(path);
+    let out_path = out_dir.join(path);
 
     let template_path_string = template_path.display().to_string();
     print_info!(true, "loading template from `{}'", template_path_string);
@@ -90,7 +127,26 @@ fn generate(lang: &Lang, path: &Path) -> Result<()> {
 
     let content = content.replace("$LIB_DIR", &libdir_escaped(&lang));
     let content = content.replace("$GDB_PATH", &gdbpath_escaped());
-    create_and_write_file(path, &content)
+    create_and_write_file(&out_path, &content)
+}
+
+fn validate_arguments(config: &ConfigFile, cmdargs: CmdLineArgs) -> Result<Project> {
+    let name = cmdargs.name;
+
+    // generate source code
+    let specified_lang = cmdargs
+        .lang
+        .unwrap_or(config.init_default_file_type.clone());
+
+    let lang = langs::FILETYPE_ALIAS
+        .get(&*specified_lang)
+        .ok_or(Error::new(ErrorKind::UnknownFileType(specified_lang)))?;
+
+    let lang = langs::LANGS_MAP
+        .get(lang)
+        .expect(&format!("internal error: unknown file type {}", lang));
+
+    Ok(Project { name, lang })
 }
 
 fn create_and_write_file(path: &Path, content: &str) -> Result<()> {
