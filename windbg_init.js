af3eb3526ae388b48c461d93fcad46f55ed0057a
statiolake <statiolake@gmail.com>
2018-11-27 15:38:06 +0900

    add windbg init script

---
+++ windbg_init.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+function initializeScript()
+{
+    return [new host.apiVersionSupport(1, 3)];
+}
+
+function invokeScript()
+{
+    //
+    // Insert your script content here.  This method will be called whenever the script is
+    // invoked from a client.
+    //
+    // See the following for more details:
+    //
+    //     https://aka.ms/JsDbgExt
+    //
+    var ctl = host.namespace.Debugger.Utility.Control; 
+    ctl.ExecuteCommand("bp main!main");
+    ctl.ExecuteCommand("g");
+}
