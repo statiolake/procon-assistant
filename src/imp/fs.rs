ff0215923abb3dd8f15ec06048a57e74a55b8270
statiolake <statiolake@gmail.com>
2020-04-07 22:57:52 +0900

    unify functionality of creating directories

--- src/imp/fs.rs
+++ src/imp/fs.rs
@@ -1,9 +1,44 @@
 use anyhow::{Context, Result};
+use if_chain::if_chain;
 use std::cmp::Ordering;
+use std::env;
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+/// Creates directories under `root`. If `override_if_cwd` is `true` and current
+/// working directory name is equal to the root's name, the root directory is
+/// not created (the current directory is assumed to be the root).  Returns
+/// actual root directory.
+pub fn create_dirs(
+    root: impl AsRef<str>,
+    dirnames: &[impl AsRef<str>],
+    override_if_cwd: bool,
+) -> Result<PathBuf> {
+    let root = root.as_ref();
+    let cwd = env::current_dir().expect("critical error: failed to get current dir");
+    let root = if_chain! {
+        if override_if_cwd;
+        if let Some(name) = cwd.file_name();
+        if name == root;
+        then {
+            Path::new(".")
+        } else {
+            Path::new(root)
+        }
+    };
+
+    fs::create_dir_all(root)
+        .with_context(|| format!("failed to create a directory `{}`", root.display()))?;
+    for dirname in dirnames {
+        let path = root.join(dirname.as_ref());
+        fs::create_dir_all(&path)
+            .with_context(|| format!("failed to create a directory `{}`", path.display()))?;
+    }
+
+    Ok(root.to_path_buf())
+}
 
 /// Write the text into the specified file unless the file doesn't exist.  If
 /// exists, return error.
