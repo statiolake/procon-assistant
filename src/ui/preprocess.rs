ef32ee095c8ca3a42efa7245c45896e29de370b0
statiolake <statiolake@gmail.com>
2020-04-15 01:30:40 +0900

    support MinifyMode in the new preprocessor

--- src/ui/preprocess.rs
+++ src/ui/preprocess.rs
@@ -1,4 +1,4 @@
-use crate::imp::config::CONFIG;
+use crate::imp::config::{MinifyMode, CONFIG};
 use crate::imp::langs;
 use crate::imp::langs::Preprocessed;
 use crate::ExitStatus;
@@ -6,7 +6,10 @@ use anyhow::{Context, Result};
 
 #[derive(clap::Clap)]
 #[clap(about = "Preprocesses current source file and prepares to submit")]
-pub struct Preprocess;
+pub struct Preprocess {
+    #[clap(short, long, help = "Minify mode")]
+    minify: Option<MinifyMode>,
+}
 
 impl Preprocess {
     pub fn run(self, _quiet: bool) -> Result<ExitStatus> {
@@ -14,7 +17,7 @@ impl Preprocess {
             langs::guess_lang().context("failed to guess the language used in this project")?;
         let source = lang.get_source().context("failed to read the sorce file")?;
         let Preprocessed(pped) = lang
-            .preprocess(&source, CONFIG.clip.minify)
+            .preprocess(&source, self.minify.unwrap_or(CONFIG.clip.minify))
             .context("failed to preprocess the source")?;
 
         println!("{}", pped);
